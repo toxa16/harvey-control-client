e9637c49cf9538aa33f96aee160f860acdb4b1e9
toxa16 <toxa16@gmail.com>
2019-10-27 17:12:18 +0200

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Navbar />
     </div>
   );
 }
