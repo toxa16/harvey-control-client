04d89a92f0563575c731c5ecc4bfc9742cd9a7f9
toxa16 <toxa16@gmail.com>
2019-10-27 18:14:46 +0200

    root saga init websocket channel

--- src/root-saga.js
+++ src/root-saga.js
@@ -1,4 +1,37 @@
+import { call, take } from 'redux-saga/effects';
+import { eventChannel } from 'redux-saga';
+
+function websocketChannel(socket) {
+  return eventChannel(emit => {
+    const onMessage = e => {
+      const message = e.data;
+      emit(JSON.parse(message));
+    };
+    const onError = err => {
+      console.error('websocket error');
+      console.error(err);
+    }
+
+    socket.addEventListener('error', onError);
+    socket.addEventListener('message', onMessage);
+
+    return () => {
+      socket.removeEventListener('error', onError);
+      socket.removeEventListener('message', onMessage)
+    }
+  });
+}
+
+function* watchChannel(channel) {
+  while (true) {
+    const action = yield take(channel);
+    console.log(action);
+  }
+}
+
 export default function* rootSaga() {
   const url = 'ws://localhost:3001';  // env
   const socket = new WebSocket(url);
+  const channel = yield call(websocketChannel, socket);
+  yield call(watchChannel, channel);
 }
