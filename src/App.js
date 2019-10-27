5134fc425ad093936263f8c161279faa0a771f06
toxa16 <toxa16@gmail.com>
2019-10-27 17:25:30 +0200

    init control panel ui

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import ControlPanel from './control-panel/ui/ControlPanel';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <ControlPanel />
     </div>
   );
 }
---
+++ src/control-panel/ui/ControlPanel.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function ControlPanel() {
+  return (
+    <div className="pt-5">
+      <div className="container">
+        <div className="text-center">
+          <h1>Control Panel</h1>
+        </div>
+        <div className="mt-5">
+          <span>Machine online status:</span>
+          {' '}
+          <span className="text-success text-uppercase font-weight-bold">Online</span>
+        </div>
+        <div className="mt-5">
+          <p>Power On/Off:</p>
+          <button className="btn btn-lg btn-danger">On/Off</button>
+          <div className="mt-3">
+            <span>Power status:</span>
+            {' '}
+            <span className="text-danger">Standby</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
